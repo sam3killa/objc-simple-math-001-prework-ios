f34c59d4ab58d4d363302e43e135b75828cf0386
sam3killa <samuels.shih@gmail.com>
2016-01-06 11:19:02 -0800

    Practiced Basic Math

--- objc-simple-math/FISAppDelegate.m
+++ objc-simple-math/FISAppDelegate.m
@@ -13,6 +13,38 @@
      
      */
     
+    // Operations with Values
+    NSInteger i = 0;
+    NSLog(@"%li",i);
+    i = 1 + 1;
+    NSLog(@"%li",i);
+    i = 8 * 3;
+    NSLog(@"%li",i);
+    
+    // Operations with Variables
+    NSInteger a = 0;
+    NSInteger b = 0;
+    NSInteger c = a + b;
+    NSLog(@"a: %li, b: %li, c: %li", a, b, c);
+    
+    a = 17;
+    b = 29;
+    NSLog(@"a: %li, b: %li, c: %li", a, b, c);
+    c = a + b;
+    NSLog(@"a: %li, b: %li, c: %li", a, b, c);
+
+    NSUInteger u = 1;
+    NSLog(@"%lu",u);
+    
+    BOOL threeisEqualToThree = 3 == 3;
+    NSLog(@"3 == 3: %d", threeisEqualToThree);
+    
+    BOOL fourIsEqualToThree = 4 == 3;
+    NSLog(@"4 == 3: %d", fourIsEqualToThree);
+    
+    CGFloat f = 2.9;
+    NSLog(@"f: %f", f);
+
     // do not alter
     return YES;  //
     ///////////////
